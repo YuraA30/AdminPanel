a54c5c8131c592bfa55b70c1d8b2ace83d3342b2
sviatikSta <apelsinovych@gmail.com>
2021-02-17 16:09:50 +0200

    json

--- server/routes/edit.js
+++ server/routes/edit.js
@@ -4,7 +4,7 @@ const db = require("../config/db");
 
 router.get("/:product_id", (req, res) => {
   db.query(
-    `SELECT * FROM products WHERE id = ${req.params.product_id}`,
+    `SELECT *,specs.name AS specs,specs.id AS specs_id FROM specs INNER JOIN products WHERE product_id = ${req.params.product_id} GROUP BY specs.name`,
     (err, result) => {
       if(err) console.log(err)
       res.json(result);
@@ -13,10 +13,9 @@ router.get("/:product_id", (req, res) => {
   );
 })
 
-router.post("/:product_id", (req, res) => {
+router.post("/:product_id", async (req, res) => {
   json = JSON.parse(JSON.stringify(req.body));
   let product = {
-    subcategory_id : req.params.cat,
     name: json.name,
     brand: json.brand,
     description: json.description,
@@ -24,10 +23,34 @@ router.post("/:product_id", (req, res) => {
     price: json.price,
     count: json.count,
   };
-  let sql = "Update user set ? where id = ${req.params.product_id}";
-  db.query(sql, product, (err, result) => {
+
+  db.query(`UPDATE products set ? where id = ${req.params.product_id}`, product, (err, result) => {
     if (err) throw err;
-    res.redirect(req.header('Referer'))
+    // res.redirect(req.header('Referer'))
+    db.query(
+      `SELECT * FROM specs WHERE subcategory_id = ${req.params.product_id} GROUP BY name`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(req.body);
+        var i = 0;
+        result.forEach((element) => {
+          let specs = {
+            subcategory_id: req.params.cat,
+            product_id: insertId,
+            name: element.name,
+            value: req.body['spec_' + element.id],
+          };
+          let sql = `UPDATE specs set ? where product_id = ${req.params.product_id}`;
+          db.query(sql, specs, (err, result) => {
+            if (err) throw err;
+            console.log(i);
+            i++;
+          });
+        });
+      }
+    );
   });
 });
 
